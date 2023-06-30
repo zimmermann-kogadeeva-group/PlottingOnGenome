4bfb861aa49ec5a880778c542e9152b9f03fdeec
Bartosz Bartmanski <bartoszbartmanski@gmail.com>
2023-06-30 09:56:55 +0200

    Incremented version number.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plotting_on_genome",
-    version="0.0.4",
+    version="0.0.5",
     author="Bartosz Bartmanski",
     author_email="bartosz.bartmanski@embl.de",
     description="Package for making genome related plots",
