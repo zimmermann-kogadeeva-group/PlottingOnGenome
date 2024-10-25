fb927dafd677c76cfeb861fe19db0548b99a0562
Bartosz Bartmanski <bartosz.bartmanski@embl.de>
2024-10-25 18:02:54 +0200

    Bump version 0.2.3 -> 0.2.4

--- src/plotting_on_genome/__init__.py
+++ src/plotting_on_genome/__init__.py
@@ -1,4 +1,4 @@
 from .main import Insert, Pipeline, download_genome, run_blast
 from .plotting import plot_insert, plot_genome, plot_histogram, plot_dists
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
